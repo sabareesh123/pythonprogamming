4e88a7ac1ab9df568233fc83fb3798a137479e8e
sabareesh123 <51222788+sabareesh123@users.noreply.github.com>
2019-07-12 17:37:49 +0530

    Update p17.py

--- p17.py
+++ p17.py
@@ -1,9 +1,11 @@
-sk=int(input())
-li=[]
-sum=0
-for digit in str(sk):
-    li.append(int(digit))		      #append the list by explicit conversion of each character
-digit_count=len(li)						        #to an integer 
-for number in range(digit_count):
-    sum+=(li[number]**digit_count)	      #add all the elements by raising all elements to the length of list
-print("yes" if sum==s else "no")
+yz=int(input())
+mg=0
+temp=yz
+while(temp>0):
+  dig=temp%10
+  mg=mg+dig ** 3
+  temp=temp//10
+if(mg==yz):
+  print("yes")
+else:
+  print("no")
