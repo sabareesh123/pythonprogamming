61883ce3624d91e0ee477624103606c5db8b7599
sabareesh123 <51222788+sabareesh123@users.noreply.github.com>
2019-07-16 14:57:51 +0530

    Create p35.py

---
+++ p35.py
@@ -0,0 +1,8 @@
+test_string=input(" ")
+t=0
+for word in test_string:
+  
+  if word=='1'or word=='2'or word=='3'or word=='4'or word=='5'or word=='6'or word=='7'or word=='8'or word=='9':
+    t+=1
+  
+print(t) 
