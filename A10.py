4e4ca9e40c271dadac1156f3612d9e329fe85f5e
sabareesh123 <51222788+sabareesh123@users.noreply.github.com>
2019-07-23 02:23:42 -0700

    Update A10.py

--- A10.py
+++ A10.py
@@ -1,8 +1,11 @@
-import sys, string, math
-s3, s4 = input().split()
-k = 0
-for j in range(len(s3)) :
-    if s3[i] != s4[j] :
-        k += 1
-if k == 1 : print('yes')
-else :      print('no')
+s3,s4=input().split()
+C=0
+for i in range(0,len(s3)):
+    for j in range(0,i+1):
+        if s3[i]!=s4[j]:
+            C+=1
+            break
+if C==1:
+    print("yes")
+else:
+    print("no")
