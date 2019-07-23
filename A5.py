002ebf2d1115ddd8589bcbaec0d7361ea4b6ed2d
sabareesh123 <51222788+sabareesh123@users.noreply.github.com>
2019-07-23 01:56:50 -0700

    Create A5.py

---
+++ A5.py
@@ -0,0 +1,5 @@
+import sys, string, math
+S = input()
+a = ['','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+i = a.index(S)
+print(i)
